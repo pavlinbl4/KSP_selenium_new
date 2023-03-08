e0338fa3700c97cd58e2e8a9e65e73c684e5c7a2
evgeniy <pavlinbl4@gmail.com>
2023-03-08 21:55:28 +0300

    oop test to create different dates formats

--- Fee_in_last_month/kommersant_dates.py
+++ Fee_in_last_month/kommersant_dates.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 class KommersantDates:
     def __init__(self, day_cycle=1):
         today = datetime.today()
+        self.previous_month_number_int = int((today.replace(day=1) - timedelta(days=1)).strftime('%m'))
         self.today = today.strftime("%d.%m.%Y")
         self.previous_month_name = (today.replace(day=1) - timedelta(days=1)).strftime('%B')
         self.previous_month_last_day = (datetime.today().replace(day=1) - timedelta(days=day_cycle)).strftime(
@@ -12,3 +13,8 @@ class KommersantDates:
         self.yesterday = (today - timedelta(days=1)).strftime("%d.%m.%Y")
         self.previous_month_check_day = (datetime.today().replace(day=1) - timedelta(days=day_cycle)).strftime(
             "%d.%m.%Y")
+
+
+if __name__ == '__main__':
+    print(KommersantDates().previous_month_check_day)
+    print(KommersantDates().days_in_month)
