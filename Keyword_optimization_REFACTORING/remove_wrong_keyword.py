08e327643b8e5068ec9c5f8af08d0ce6878df705
evgeniy <pavlinl4@gmail.com>
2023-03-24 23:09:37 +0300

    remove bad keywor from keywords

---
+++ Keyword_optimization_REFACTORING/remove_wrong_keyword.py
@@ -0,0 +1,15 @@
+from Common.set_to_string import convert_set_to_string
+from Keyword_optimization_REFACTORING.make_better import keywords_opimization
+
+
+def remove_mistake(keyword, keywords):
+    good_keywords = keywords_opimization(keywords)
+    good_set = set(good_keywords.split(', '))
+    no_bad_word_set = good_set.difference(set([keyword]))
+    return convert_set_to_string(no_bad_word_set)
+
+
+
+
+if __name__ == '__main__':
+    print(remove_mistake('rabbit', 'fox, box, rabbit, beer'))
