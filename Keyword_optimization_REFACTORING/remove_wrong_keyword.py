7e3f5033564ce8935130234775696d206484e617
evgeniy <pavlinl4@gmail.com>
2023-03-25 21:20:26 +0300

    remove wrong keywords

--- Keyword_optimization_REFACTORING/remove_wrong_keyword.py
+++ Keyword_optimization_REFACTORING/remove_wrong_keyword.py
@@ -4,7 +4,7 @@ from Keyword_optimization_REFACTORING.make_better import keywords_opimization
 
 def remove_mistake(keyword, keywords):
     good_keywords = keywords_opimization(keywords)
-    good_set = set(good_keywords.split(', '))
+    good_set = set(good_keywords.split(','))
     no_bad_word_set = good_set.difference(set([keyword]))
     return convert_set_to_string(no_bad_word_set)
 
