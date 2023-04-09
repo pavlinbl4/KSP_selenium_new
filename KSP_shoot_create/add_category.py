7d3e3b8af97566dbb4a008070a0d809a58c268d1
evgeniy <pavlinbl4@gmail.com>
2023-04-09 16:52:11 +0300

    function to chose category from GUI

--- KSP_shoot_create/add_category.py
+++ KSP_shoot_create/add_category.py
@@ -1,20 +1,21 @@
-
 from Common.authorization import autorization
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
+from KSP_shoot_create.checkbox_output import create_checkbox_dict
 
 
-def open_shoot_creation_page():
+def open_shoot_creation_page(browser):
     browser.find_element(By.CSS_SELECTOR,
                          "body > table.logotbl > tbody > tr:nth-child(3)"
                          " > td > table > tbody > tr > td:nth-child(2) > a").click()
     browser.find_element(By.ID,
                          "nav_shoots_change").click()
+    return browser
 
 
-def select_category(category_number):
+def select_category(category_number, browser):
     wait = WebDriverWait(browser, 10)
     browser.execute_script("OpenPopupWindow('ShootSubjectsAdmin.asp')")
     wait.until(EC.number_of_windows_to_be(2))
@@ -25,9 +26,17 @@ def select_category(category_number):
     browser.find_element(By.XPATH, '//*[@id="DivSubmit"]/input[1]').click()
 
 
-if __name__ == '__main__':
+def add_category_to_shoot():
+    category_number = create_checkbox_dict()
     browser = autorization()
-    open_shoot_creation_page()
-    select_category('4000000')
+    browser = open_shoot_creation_page(browser)
+    select_category(category_number, browser)
 
+
+
+
+
+
+if __name__ == '__main__':
+    add_category_to_shoot()
 
