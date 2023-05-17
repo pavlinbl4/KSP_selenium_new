24206121c2d6c0035ed9bd03f0f98138169e20c0
evgeniy <pavlinbl4@gmail.com>
2023-05-17 12:31:31 +0300

    it was an error in regex mask. PhotoID may by shorter 7 digits.

--- Fee_in_last_month/images_vocabulary.py
+++ Fee_in_last_month/images_vocabulary.py
@@ -5,7 +5,12 @@ def make_images_voc(images_links):
     images_voc = {}
     for i in images_links:
         ksp_id = re.findall(r'(?<=photocode=)\w{16}', str(i))[0]
-        regex = r'(?<=photoid=)\d{7}(?=\")'
-        photoid = re.findall(regex, str(i))[0]
-        images_voc[photoid] = ksp_id
+        regex = r'(?<=photoid=)\d{5,7}(?=\")'
+        try:
+            photoid = re.findall(regex, str(i))[0]
+        except IndexError:
+            print(str(i))
+        else:
+            images_voc[photoid] = ksp_id
+
     return images_voc
