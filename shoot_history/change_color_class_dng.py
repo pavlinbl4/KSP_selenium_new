9412def987f3fa3215e85b92b41e148384bfa5ea
evgeniy_MacBook <pavlinbl4@gmail.com>
2023-06-24 18:45:45 +0300

    not it work on silicon

--- shoot_history/change_color_class_dng.py
+++ shoot_history/change_color_class_dng.py
@@ -1,12 +1,16 @@
 import exiftool
 
 
-def change_color_class(file, image_title):
+def change_color_class(file, image_title, color):
     with exiftool.ExifToolHelper() as et:
         et.set_tags(
             [file],
             tags={"XMP:Title": image_title,
-                  'XMP:Label': "Red",
+                  'XMP:Label': color,
                   'XMP:Creator': 'Eugene Pavlenko'},
             params=["-P", "-overwrite_original"]
         )
+
+if __name__ == '__main__':
+    file = '/Volumes/big4photo/python_test_images/20151102__PAV5906.nef'
+    change_color_class(file, 'image_title', color='Red"')
