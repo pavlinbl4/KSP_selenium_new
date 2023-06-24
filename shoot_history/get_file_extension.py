355af9a79b2ab03bf8fc375609782b097af4fdd3
evgeniy_MacBook <pavlinbl4@gmail.com>
2023-06-24 19:04:26 +0300

    it was a problem with digit '1 in file extension'

--- shoot_history/get_file_extension.py
+++ shoot_history/get_file_extension.py
@@ -2,6 +2,10 @@ import re
 
 
 def get_file_extension(path_to_file):
-    re_pattern = r'(?<=\.)[A-Za-z]{3,4}'
+    re_pattern = r'(?<=\.)[1A-Za-z]{3,4}'
     extension = re.findall(re_pattern, path_to_file)[0]
     return extension  # string with file extension
+
+
+if __name__ == '__main__':
+    get_file_extension('/Users/evgeniy/Pictures/2023/20230613_Брега - мегалит/20230613EPAV7906.on1')
