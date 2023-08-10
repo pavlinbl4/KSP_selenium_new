90a38bbfe6ba3b1e5844bcd8fe2aff8d2f9c7095
evgeniy <pavlinbl4@gmail.com>
2023-08-10 22:16:36 +0300

    Add some logging to print out info/errors rather than just printing Use pathlib for file paths instead of strings

--- shoot_history/change_color_class.py
+++ shoot_history/change_color_class.py
@@ -2,52 +2,60 @@ import pyexiv2
 import os
 from colorama import Fore
 import exiftool
+import logging
+import pathlib
+
+logger = logging.getLogger(__name__)
 
 
-def change_color_class_raw(file, image_title):
-    changes = {}
+def change_color_class_raw(image_path, image_title):
+    """Modify RAW image metadata using pyexiv2"""
+    changes = {
+        'Xmp.dc.creator': 'Eugene Pavlenko',
+        'Xmp.xmp.Label': 'Red',
+        'Xmp.dc.title': {'lang="x-default"': image_title}
+    }
     try:
-        with open(file, 'rb+') as image_file:
-
-            with pyexiv2.ImageData(image_file.read()) as meta_data:
-                # data = meta_data.read_xmp()
-                # changes['Xmp.photomechanic.ColorClass'] = 2
-                # changes['Xmp.photoshop.Urgency'] = 2
-                # changes['Xmp.dc.rights'] = {'lang="x-default"': 'pavlinbl4'}
-                # changes['Xmp.photoshop.Credit'] = 0
-                changes['Xmp.dc.creator'] = ['Eugene Pavlenko']
-                changes['Xmp.xmp.Label'] = "Red"
-                changes['Xmp.dc.title'] = {'lang="x-default"': image_title}
-                # changes['Xmp.dc.description'] = {'lang="x-default"': "CAPTION"}
-                # changes['Xmp.dc.subject'] = ['KEYWORD',"keywords"]
-                meta_data.modify_xmp(changes)
+        with image_path.open('rb+') as image_file:
+            with pyexiv2.ImageData(image_file.read()) as metadata:
+                metadata.modify_xmp(changes)
                 image_file.seek(0)
                 image_file.truncate()
-                image_file.write(meta_data.get_bytes())
-            image_file.seek(0)
+                image_file.write(metadata.get_bytes())
     except Exception as ex:
+        logger.error("Error modifying %s: %s", image_path.name, ex)
         print(f'{Fore.RED}{ex}{Fore.RESET}in file {Fore.GREEN}{os.path.basename(file)}{Fore.RED}')
 
 
+def change_color_class_dng(image_path, image_title, color):
+    """Modify DNG image metadata using ExifTool"""
 
-
-
-def change_color_class_dng(file, image_title):
     try:
         with exiftool.ExifToolHelper() as et:
             et.set_tags(
                 [file],
                 tags={"XMP:Title": image_title,
-                      'XMP:Label': "Red",
+                      'XMP:Label': color,
                       'XMP:Creator': 'Eugene Pavlenko'},
                 params=["-P", "-overwrite_original"]
             )
     except Exception as ex:
+        logger.error("Error modifying %s: %s", image_path.name, ex)
         print(f'{Fore.RED}{ex}{Fore.RESET}in file {Fore.GREEN}{os.path.basename(file)}{Fore.RED}')
 
 
+if __name__ == '__main__':
+    # file = '/Volumes/big4photo/python_test_images/20151102__PAV5906.nef'
+    file = '/Volumes/big4photo/python_test_images/20230403EPAV9047.ORF'
+    # file = '/Volumes/big4photo-3/2019/10_October/20191007_Недвижимость/20191007EPAV7002.ORF'
+    # file = '/Volumes/big4photo/python_test_images/20230526PEV_7378.JPG'
+    # file = '/Volumes/big4photo/python_test_images/20230602_PAV5801.DNG'
+    # change_color_class_dng(file, 'edit_info_with_ExifToolHelper', color="Blue")
 
+    t_image_path = pathlib.Path(file)
 
-# file = "/Volumes/big4photo-4/2022/06_June/20220615_ПМЭФ/20220615PEV_3405.DNG"
-# image_title = 'test'
-# change_color_class(file, image_title)
+    change_color_class_dng(
+        t_image_path,
+        image_title='New title',
+        color='Blue'
+    )
