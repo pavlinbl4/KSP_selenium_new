b22dc3572b0e9cfc4a1ac6073d45609b6a9d1509
evgeniy <pavlinbl4@gmail.com>
2023-08-10 22:24:35 +0300

    Use more descriptive variable names like image_path instead of just file

--- shoot_history/change_color_class_dng.py
+++ shoot_history/change_color_class_dng.py
@@ -1,15 +1,16 @@
 import exiftool
 
 
-def change_color_class(file, image_title, color):
+def change_color_class(image_path, image_title, color):
     with exiftool.ExifToolHelper() as et:
         et.set_tags(
-            [file],
+            [image_path],
             tags={"XMP:Title": image_title,
                   'XMP:Label': color,
                   'XMP:Creator': 'Eugene Pavlenko'},
             params=["-P", "-overwrite_original"]
         )
+
 
 if __name__ == '__main__':
     file = '/Volumes/big4photo/python_test_images/20151102__PAV5906.nef'
