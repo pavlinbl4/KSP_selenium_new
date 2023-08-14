3d0ae329ba979158349766f2e2fef3b71638ca53
evgeniy <pavlinbl4@gmail.com>
2023-08-14 13:52:05 +0300

    refactoring

--- Fee_in_last_month/images_links.py
+++ Fee_in_last_month/images_links.py
@@ -4,3 +4,8 @@ from bs4 import BeautifulSoup
 def get_image_links(html):
     soup = BeautifulSoup(html, 'lxml')
     return soup.find_all('table')[9].find('tbody').find_all(title="Добавить кадрировку")
+
+
+
+if __name__ == '__main__':
+    get_image_links('/Volumes/big4photo/Documents/Kommersant/FEE/7_July_fee/HTML/source_page_13.07.2023.html')
