ed174c45f73de9a254f6fee43b3716c65aa03419
evgeniy_MacBook <pavlinbl4@gmail.com>
2023-08-15 00:05:27 +0300

    added my name in image search

--- KSP_shoot_create/find_images.py
+++ KSP_shoot_create/find_images.py
@@ -1,13 +1,25 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from Common.authorization import autorization
 
 
-def find_images_by_id(shoot_id, browser):  # авторизация гна главной странице
-    browser.find_element(By.CSS_SELECTOR, '#code').clear()
-    browser.find_element(By.CSS_SELECTOR, '#code').send_keys(shoot_id)
-    browser.find_element(By.CSS_SELECTOR, '#lib0').click()
-    select = Select(browser.find_element(By.NAME, 'ps'))
+def find_images_by_id(shoot_id, driver):  # авторизация гна главной странице
+
+    driver.find_element(By.CSS_SELECTOR, '#au').clear()
+    driver.find_element(By.CSS_SELECTOR, '#au').send_keys('Евгений Павленко')
+
+    driver.find_element(By.CSS_SELECTOR, '#code').clear()
+    driver.find_element(By.CSS_SELECTOR, '#code').send_keys(shoot_id)
+    driver.find_element(By.CSS_SELECTOR, '#lib0').click()
+    select = Select(driver.find_element(By.NAME, 'ps'))
     select.select_by_value('100')
-    browser.find_element(By.CSS_SELECTOR, '#searchbtn').click()
-    link = browser.current_url
+    driver.find_element(By.CSS_SELECTOR, '#searchbtn').click()
+    link = driver.current_url
     return link
+
+
+if __name__ == '__main__':
+    driver = autorization()
+    find_images_by_id('KSP_017270', driver)
+    driver.close()
+    driver.quit()
