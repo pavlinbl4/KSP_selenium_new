1f668648705339463b008c1c4e6e2442ea630472
evgeniy <pavlinbl4@gmail.com>
2023-08-14 22:59:58 +0300

    try to change time sleep to WebDriverWait but no good result

--- KSP_shoot_create/create_shoot.py
+++ KSP_shoot_create/create_shoot.py
@@ -9,54 +9,65 @@ from Common.notification import system_notification
 from KSP_shoot_create.add_category import select_category
 from KSP_shoot_create.checkbox_output import create_checkbox_dict
 from KSP_shoot_create.input_window import get_input_data
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def create_shoot():
     today_date = f'{datetime.now().strftime("%d.%m.%Y")}'
-    # shoot_caption = input("Введите описание съемки\n")
-    shoot_caption = get_input_data()
-    category_number = create_checkbox_dict()
-    browser = autorization()
+
+    shoot_caption = get_input_data()  # add caption via GUI
+    category_number = create_checkbox_dict()   # select category from GUI
+    driver = autorization()
     try:
-        # author_input = browser.find_element(By.CSS_SELECTOR, "input#au")
-        # author_input.send_keys("Евгений Павленко")
-        # переход в режим описания съемки
-        browser.find_element(By.CSS_SELECTOR,
+
+        driver.find_element(By.CSS_SELECTOR,
                              "body > table.logotbl > tbody > tr:nth-child(3)"
                              " > td > table > tbody > tr > td:nth-child(2) > a").click()
-        browser.find_element(By.ID,
+        driver.find_element(By.ID,
                              "nav_shoots_change").click()
 
+        original_window = driver.current_window_handle
+
+        # add category
+        select_category(category_number, driver)
+
+        driver.switch_to.window(original_window)  # focus in the main window after closing category window
+
         # добавляю описание съемки
-        caption_input = browser.find_element(By.ID, "ShootDescription")
+        caption_input = driver.find_element(By.ID, "ShootDescription")
         caption_input.send_keys(shoot_caption)
 
         # ввожу дату
-        day_input = browser.find_element(By.ID, "DateFrom")
+        day_input = driver.find_element(By.ID, "DateFrom")
         day_input.send_keys(today_date)
 
-        time_input = browser.find_element(By.ID, 'TimeFrom')
+        time_input = driver.find_element(By.ID, 'TimeFrom')
         time_input.send_keys(Keys.NUMPAD1)
 
         time_input.send_keys(Keys.SPACE)
 
-        time_input = browser.find_element(By.ID, 'TimeTo')
+        time_input = driver.find_element(By.ID, 'TimeTo')
         time_input.send_keys(Keys.NUMPAD2)
 
         time_input.send_keys(Keys.SPACE)
 
         # выпадающее меню выбираю с помощью кнопок клавиатуры
-        customer_input = browser.find_element(By.ID, "CustomerContact")
+        WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.ID, "CustomerContact")))
+        customer_input = driver.find_element(By.ID, "CustomerContact")
         customer_input.send_keys("Павленко Евгений Валентинович")
+
         time.sleep(1)
+        # WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.ID, "CustomerContact")))
         customer_input.send_keys(Keys.DOWN)
         customer_input.send_keys(Keys.ENTER)
+        # WebDriverWait(driver, 1).until(EC.staleness_of(customer_input))
 
         # выбираю бильдредактора с помощью класса Select
-        select = Select(browser.find_element(By.NAME, 'EditorContactID'))
+        select = Select(driver.find_element(By.NAME, 'EditorContactID'))
         select.select_by_value('2571')
 
-        author_input = browser.find_element(By.ID, "AuthorContact")
+        author_input = driver.find_element(By.ID, "AuthorContact")
         author_input.send_keys("Павленко Евгений Валентинович")
         time.sleep(1)
         author_input.send_keys(Keys.DOWN)
@@ -64,27 +75,28 @@ def create_shoot():
         author_input.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        # add category
-        select_category(category_number, browser)
-
         # confirm shoot creation
-        # browser.find_element(By.ID, 'SubmitBtn').click()
+        # driver.find_element(By.ID, 'SubmitBtn').click()
 
-        number = browser.find_element(By.ID, "shootnum").text
+
+        number = driver.find_element(By.ID, "shootnum").text
         number = number.replace("№ ", "KSP_0")
         pyperclip.copy(number)
 
         system_notification(number, shoot_caption)
 
+        driver.close()
+        driver.quit()
+
         time.sleep(1)
 
     except Exception as ex:
         print(ex)
-        browser.close()
-        browser.quit()
-    finally:
-        browser.close()
-        browser.quit()
+        driver.close()
+        driver.quit()
+    # finally:
+    #     driver.close()
+    #     driver.quit()
 
 
 if __name__ == '__main__':
