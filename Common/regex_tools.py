eb788f9e8ee0ea77a2b6e5b98aaf2cce503c2988
evgeniy_MacBook <pavlinbl4@gmail.com>
2023-08-15 11:44:04 +0300

    all regex tools

---
+++ Common/regex_tools.py
@@ -0,0 +1,7 @@
+import re
+
+def make_text_edit_link(link):
+    inner_id = re.findall(r'(?<=id=)\d+', link)[0]
+    image_id = re.findall(r'(?<=photocode=)[^&]+', link)[0]
+    image_edit_link = f'https://image.kommersant.ru/photo/archive/ViewPhoto.asp?ID={inner_id}&Lang=1&L=1'
+    return image_edit_link, image_id, inner_id
