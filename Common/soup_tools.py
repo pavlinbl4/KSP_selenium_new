0b69f914785eb58be1cd319993cc887c5b1e4b0d
evgeniy <pavlinbl4@gmail.com>
2023-08-16 10:46:33 +0300

    .

--- Common/soup_tools.py
+++ Common/soup_tools.py
@@ -1,4 +1,6 @@
 from bs4 import BeautifulSoup
+import re
+
 # pip install beautifulsoup4
 # pip install lxml
 
@@ -8,3 +10,14 @@ def get_image_links(html):
     tbody = table.find('tbody')
     images_links = tbody.find_all(title="Добавить кадрировку")
     return images_links
+
+
+def get_total_images(html):  # get number of images in shoot
+    soup = BeautifulSoup(html, 'lxml')
+    total_images = soup.find('span', id='ctl00_MainContent_AllPhoto1').text
+    total_images = int(re.findall(r'\d+', total_images)[0])  # количество файлов в съемке
+    return total_images
+
+
+def get_soup(html):
+    return BeautifulSoup(html, 'lxml')
