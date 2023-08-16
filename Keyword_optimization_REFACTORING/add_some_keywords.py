3c1547aac45c3648665c13b6ae954abfbd5f1f48
evgeniy <pavlinbl4@gmail.com>
2023-08-16 10:10:35 +0300

    refactoring

--- Keyword_optimization_REFACTORING/add_some_keywords.py
+++ Keyword_optimization_REFACTORING/add_some_keywords.py
@@ -2,13 +2,13 @@ from Common.set_to_string import convert_set_to_string
 from Keyword_optimization_REFACTORING.make_better import keywords_opimization
 
 
-def add_new_keywords(new_keywords, keywords):
+def add_new_keywords(new_keywords: str, keywords: str) -> str:
     good_keywords = keywords_opimization(keywords)
-    good_set = set(good_keywords.split(','))
-    new_keywords_set = set(new_keywords.split(','))
+    good_set = set(word.strip() for word in good_keywords.split(','))
+    new_keywords_set = set(word.strip() for word in new_keywords.split(','))
     apdated_keywords = good_set.union(new_keywords_set)
     return convert_set_to_string(apdated_keywords)
 
 
 if __name__ == '__main__':
-    print(add_new_keywords(' water   ,      beer,   bread', 'cheese, bread'))
+    print(add_new_keywords(' new_kyeword_1   ,      new_keyword_2,   new_keyword_3', 'old1, old2'))
