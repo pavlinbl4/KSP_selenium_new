7204d48775599c288fd486c2e2bb889427e047ae
evgeniy <pavlinbl4@gmail.com>
2023-08-16 10:06:32 +0300

    refactoring

--- shoot_history/lost_files_to_csv.py
+++ shoot_history/lost_files_to_csv.py
@@ -1,13 +1,13 @@
-from check_file_exist import create_file_if_no
 from csv_writer import csv_writer
+from shoot_history.check_file_exist import create_file_if_not_exists
 
 
 def write_lost_files_info(original_file_name, photo_id):
-    csv_file = create_file_if_no('/Volumes/big4photo/Documents/Kommersant/shoot_rename', 'lost_images.csv')
+    csv_file = create_file_if_not_exists('Kommersant/shoot_rename', 'lost_images.csv')
     columns_name = ['original_file_name', 'photo_id']
     info = [original_file_name, photo_id]
     csv_writer(info, columns_name, csv_file)
 
 
 if __name__ == '__main__':
-    print(write_lost_files_info('20220622PEV_4551', 'KSP_017605_00008'))
+    write_lost_files_info('XXX', 'YYY')
