6105204eecf1dab9d41b5ffd05003eeb00e1d1f2
evgeniy <pavlinbl4@gmail.com>
2023-08-16 10:05:14 +0300

    it works but dont close browser

--- shoot_history/main_shoot_history_page.py
+++ shoot_history/main_shoot_history_page.py
@@ -10,20 +10,20 @@ path_to_file = '/Users/evgeniy/Documents/Kommersant/shoot_rename/shoot_story.xls
 
 shoot_id = chose_input()
 
-# path = select_folder()
-path = '/Users/evgeniy/Pictures'
-browser = autorization()
+path = select_folder()
+# path = '/Users/evgeniy/Pictures'
+driver = autorization()
 
 page_link = make_page_link(shoot_id)
 
-open_page(page_link, browser)
+open_page(page_link, driver)
 
-full_history_page_source = work_to_history(browser)  # получаю данные со страницы истории
+full_history_page_source = work_to_history(driver)  # получаю данные со страницы истории
 
 save_html_page(full_history_page_source)  # временно сохраняю страницу
 
-file_renames = scrap_html(read_html(), path)
+file_renames = scrap_html(read_html(), path)  # path to folder with images
 
 write_rename_voc(path_to_file, file_renames, shoot_id)
 
-end_selenium(browser)
+end_selenium(driver)
