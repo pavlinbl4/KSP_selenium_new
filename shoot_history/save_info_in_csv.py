2582474fcdf8e3c581e8a5b74dbff02be940ef6c
evgeniy <pavlinbl4@gmail.com>
2023-08-17 19:36:51 +0300

    csv tools

--- shoot_history/save_info_in_csv.py
+++ shoot_history/save_info_in_csv.py
@@ -1,5 +1,18 @@
 from shoot_history.check_file_exist import create_file_if_not_exists
-from shoot_history.csv_writer import csv_writer
+import os
+import csv
+
+def csv_writer(info, columns_name, csv_file_path):
+    def write_csv_file(info, csv_file_path):
+        with open(csv_file_path, 'a') as input_file:
+            writer = csv.writer(input_file)
+            writer.writerow(info)
+
+    if os.path.exists(csv_file_path):
+        write_csv_file(info, csv_file_path)
+    else:
+        info = columns_name
+        write_csv_file(info, csv_file_path)
 
 
 def write_lost_files_info(original_file_name, photo_id):
