2bbd07f18327a04cde88e3540a0b0dcd6a35f840
evgeniy_MacBook <pavlinbl4@gmail.com>
2023-08-17 10:15:17 +0300

    add new function

--- Common/selenium_tools.py
+++ Common/selenium_tools.py
@@ -1,12 +1,19 @@
 from selenium.webdriver.common.by import By
 import time
 
-from Common.regex_tools import make_text_edit_link
+from Common.kp_image_info_page import grab_image_info_page
+from Common.regex_tools import make_text_edit_link, replace_to_comma
 from Common.soup_tools import get_image_links
 
 red = '\033[91m'
 green = '\33[32m'
 end = '\033[0m'
+
+
+def set_keywords_to_site(good_keywords, driver):
+    driver.find_element(By.NAME, 'KeywordsRus').send_keys(good_keywords)
+    driver.find_element(By.NAME, 'Add').click()
+
 
 def end_selenium(driver):
     driver.close()
@@ -40,24 +47,17 @@ def check_keywords_number(keyword, driver):  # take number of images from site
 def images_rotator(images_number, keyword_link, driver):
     range_number = images_number // 100 + 2  # количиство страниц выданных поиском
     # for x in range(1, range_number):  # главный цикл работы программы
-    for x in range(1, 3):  # главный цикл работы программы
+    for x in range(2, 3):  # главный цикл работы программы
         link = f'{keyword_link}2&pg={x}'
         html = go_my_images(link, keyword='', driver=driver)  # получаю html  открытой страницы
         images_links = get_image_links(html)  # получаю список ссылок редактирование изображения
         print(f'на странице {x} - {len(images_links)} снимков')
 
         for i in range(len(images_links)):  # (len(images_links)):
-            text_edit_link, image_id, inner_id = make_text_edit_link(images_links[i].get('href')) # generate edit image link
-            driver.get(text_edit_link)
-            time.sleep(1)
+            text_edit_link, image_id, inner_id = make_text_edit_link(
+                images_links[i].get('href'))  # generate edit image link
+            image_id, caption, keywords = grab_image_info_page(driver, text_edit_link)  # grab info
 
-            # start here function that would grab important information from edit image page
-
-
-            # Access denied. - use try/exept
-            keywords = driver.find_element(By.NAME, 'KeywordsRus').text  # copy keywords from field
-            print(keywords)
-
-
-
-
+            # if keywords not empty - optimise it
+            if keywords != '':
+                optimized_keywords = replace_to_comma(keywords)
