f4de68e6d9792993cfa10a8905c9821e9243a44d
evgeniy_MacBook <pavlinbl4@gmail.com>
2023-08-17 09:42:20 +0300

    Use ID and TAG_NAME locators which are more robust Get attribute values instead of text where applicable

--- Common/kp_image_info_page.py
+++ Common/kp_image_info_page.py
@@ -4,21 +4,18 @@ available when you press the button with the hammer and wrench icon.
 """
 
 from selenium.webdriver.common.by import By
-
 from Common.authorization import autorization
-from Common.selenium_tools import go_my_images, end_selenium
 
 
-def grab_image_info_page(driver, html):
-    driver.get(html)
-    keywords = driver.find_element(By.NAME, 'KeywordsRus').text  # copy keywords from field image info page
-    image_id = driver.find_element(By.CSS_SELECTOR, '#photoPreview > span:nth-child(2)').text
-    caption = driver.find_element(By.XPATH, '//*[@id="DescriptionRus"]').text
+def grab_image_info_page(driver, info_page_url):
+    driver.get(info_page_url)
+    keywords = driver.find_element(By.ID, 'KeywordsRus').get_attribute('value')
+    image_id = driver.find_element(By.ID, 'photoPreview').find_element(By.TAG_NAME, 'span').text
+    caption = driver.find_element(By.ID, 'DescriptionRus').get_attribute('value')
     return image_id, caption, keywords
 
 
 if __name__ == '__main__':
     t_driver = autorization()
     print(grab_image_info_page(t_driver,
-                               html='https://image.kommersant.ru/photo/archive/adm/AddPhotoStep3.asp?ID=3807024&CloseForm=1'))
-    end_selenium(t_driver)
+                               info_page_url='https://image.kommersant.ru/photo/archive/adm/AddPhotoStep3.asp?ID=3807024&CloseForm=1'))
