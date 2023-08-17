99a315df82ae4fc7a1b087c7d8d4fb48d30420c7
evgeniy_MacBook <pavlinbl4@gmail.com>
2023-08-17 12:49:21 +0300

    new cvs writer for keywords

--- shoot_history/lost_files_to_csv.py
+++ shoot_history/lost_files_to_csv.py
@@ -1,5 +1,5 @@
-from csv_writer import csv_writer
 from shoot_history.check_file_exist import create_file_if_not_exists
+from shoot_history.csv_writer import csv_writer
 
 
 def write_lost_files_info(original_file_name, photo_id):
@@ -8,6 +8,13 @@ def write_lost_files_info(original_file_name, photo_id):
     info = [original_file_name, photo_id]
     csv_writer(info, columns_name, csv_file)
 
+def write_kp_files_keywords(image_id, caption, keywords):
+    csv_file = create_file_if_not_exists('Kommersant', 'images_keywords.csv')
+    columns_name = ['image_id', 'caption', 'keywords']
+    info = [image_id, caption, keywords]
+    csv_writer(info, columns_name, csv_file)
+
 
 if __name__ == '__main__':
-    write_lost_files_info('XXX', 'YYY')
+    # write_lost_files_info('XXX', 'YYY')
+    write_kp_files_keywords('fgfgffg', '444444444', 'fgfgfgf, 7777')
