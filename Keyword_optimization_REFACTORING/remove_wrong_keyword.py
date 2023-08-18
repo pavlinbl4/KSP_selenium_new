3b73bc5c1bef1c33180ec0f26f9512ebec258382
evgeniy_MacBook <pavlinbl4@gmail.com>
2023-08-18 22:55:17 +0300

    add comments

--- Keyword_optimization_REFACTORING/remove_wrong_keyword.py
+++ Keyword_optimization_REFACTORING/remove_wrong_keyword.py
@@ -3,9 +3,9 @@ from Common.set_to_string import convert_set_to_string
 
 
 def remove_mistake(keyword, keywords):
-    good_keywords = keywords_opimization(keywords)
-    good_keywords_set = set(good_keywords.split(','))
-    no_bad_words_set = good_keywords_set - {keyword}
+    good_keywords = keywords_opimization(keywords) # optimized keywords from image on site
+    good_keywords_set = set(good_keywords.split(',')) # crete set from optimized keywords
+    no_bad_words_set = good_keywords_set - {keyword} # remove mistake from good keywords
     return convert_set_to_string(no_bad_words_set)
 
 
