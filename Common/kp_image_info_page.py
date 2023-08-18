bcce5101293355de866639ce2d756dc18579d20a
evgeniy <pavlinbl4@gmail.com>
2023-08-18 09:54:19 +0300

    modified added try/except

--- Common/kp_image_info_page.py
+++ Common/kp_image_info_page.py
@@ -3,9 +3,9 @@ Work with the image description page that is
 available when you press the button with the hammer and wrench icon.
 """
 
-from selenium.webdriver.common.by import By
 from Common.authorization import autorization
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, UnexpectedAlertPresentException
+from selenium.webdriver.common.by import By
 
 
 def grab_image_info_page(driver, info_page_url):
@@ -14,9 +14,16 @@ def grab_image_info_page(driver, info_page_url):
         keywords = driver.find_element(By.ID, 'KeywordsRus').get_attribute('value')
         image_id = driver.find_element(By.ID, 'photoPreview').find_element(By.TAG_NAME, 'span').text
         caption = driver.find_element(By.ID, 'DescriptionRus').get_attribute('value')
+
+    except UnexpectedAlertPresentException:
+        alert = driver.switch_to.alert
+        alert.accept()
+        print("Alert window")
+
     except NoSuchElementException:
         print("One of the elements was not found on the page")
         return None, None, None
+
     return image_id, caption, keywords
 
 
