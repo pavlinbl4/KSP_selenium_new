2ac822291b953897b437a685736b26594762c998
evgeniy <pavlinbl4@gmail.com>
2023-08-18 13:06:56 +0300

    image_info_optimization - work

--- Common/kp_image_info_page.py
+++ Common/kp_image_info_page.py
@@ -5,7 +5,7 @@ available when you press the button with the hammer and wrench icon.
 
 from Common.authorization import autorization
 from selenium.common.exceptions import NoSuchElementException
-from Common.regex_tools import replace_to_comma
+from Common.regex_tools import replace_to_comma, keywords_opimization
 from Common.save_info_in_csv import write_kp_files_keywords
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
@@ -20,12 +20,11 @@ def set_keywords_to_site(good_keywords, driver):
     driver.find_element(By.NAME, 'KeywordsRus').send_keys(good_keywords)
     driver.find_element(By.NAME, 'Add').click()
 
-        # wait = WebDriverWait(driver, 1)
-        # alert = wait.until(EC.alert_is_present())
-        #
-        # if alert:
-        #     alert.accept()
-
+    # wait = WebDriverWait(driver, 1)
+    # alert = wait.until(EC.alert_is_present())
+    #
+    # if alert:
+    #     alert.accept()
 
     # except TimeoutException:
     #     print('no TimeoutException')
@@ -57,7 +56,7 @@ def image_info_optimization(driver, text_edit_link):
     if keywords != '' and keywords is not None:
         write_kp_files_keywords(image_id, caption, keywords)  # save data in csv file
 
-        optimized_keywords = replace_to_comma(keywords)  # replace ; with comma
+        optimized_keywords = keywords_opimization(keywords)  # replace ; with comma
         print(optimized_keywords)
 
         set_keywords_to_site(optimized_keywords, driver)  # write optimized keywords to site
@@ -65,8 +64,6 @@ def image_info_optimization(driver, text_edit_link):
 
 if __name__ == '__main__':
     t_driver = autorization()
-    # print(grab_image_info_page(t_driver,
-    #                            info_page_url='https://image.kommersant.ru/photo/archive/adm/AddPhotoStep3.asp?ID=3807024&CloseForm=1'))
 
     image_info_optimization(t_driver,
-                            'https://image.kommersant.ru/photo/archive/adm/AddPhotoStep3.asp?ID=2582832&CloseForm=1')
+                            'https://image.kommersant.ru/photo/archive/adm/AddPhotoStep3.asp?ID=3362860&CloseForm=1')
