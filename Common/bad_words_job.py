c1be35facb66d944cdd7447ddf5847b1b55336a5
evgeniy <pavlinbl4@gmail.com>
2023-08-27 17:41:50 +0300

    refactoring

--- Common/bad_words_job.py
+++ Common/bad_words_job.py
@@ -5,14 +5,15 @@ def get_bad_words_from_txt_file(path_to_txt: str) -> str:
     return f"r'{words_to_remove}'"
 
 
-def add_bad_words_from_list(words_list,bad_word_file):
+def add_bad_words_from_list(words_list, bad_word_file):
     with open(bad_word_file, 'a') as text_file:
         for word in words_list:
             if word not in get_bad_words_from_txt_file(bad_word_file):
-                text_file.write(word + '\n')
+                text_file.write(word.lower() + '\n')
+
 
 bad_word_file = '/Users/evgeniy/Documents/keywords/bad_words.txt'
 
 if __name__ == '__main__':
     # print(get_bad_words_from_txt_file('/Users/evgeniy/Documents/keywords/bad_words.txt'))
-    add_bad_words_from_list(['bad_words_list'] )
+    add_bad_words_from_list(['bad_words_list'])
