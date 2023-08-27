eca11d80145cf8f4a5b19ae512110282ca624fda
evgeniy <pavlinbl4@gmail.com>
2023-08-27 11:13:39 +0300

    refactoring

--- KSP_shoot_create/add_category.py
+++ KSP_shoot_create/add_category.py
@@ -1,18 +1,7 @@
-from Common.authorization import autorization
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
-from KSP_shoot_create.checkbox_output import create_checkbox_dict
-
-
-# def open_shoot_creation_page(driver):
-#     driver.find_element(By.CSS_SELECTOR,
-#                          "body > table.logotbl > tbody > tr:nth-child(3)"
-#                          " > td > table > tbody > tr > td:nth-child(2) > a").click()
-#     driver.find_element(By.ID,
-#                          "nav_shoots_change").click()
-#     return driver
 
 
 def select_category(category_number, driver):
@@ -25,13 +14,3 @@ def select_category(category_number, driver):
     driver.find_element(By.XPATH, '//*[@id="addrow"]').click()
     driver.find_element(By.XPATH, '//*[@id="DivSubmit"]/input[1]').click()
     return driver
-
-# def add_category_to_shoot():
-#     category_number = create_checkbox_dict()
-#     # browser = autorization()  # this only for test
-#     # browser = open_shoot_creation_page(browser)   # this only for test
-#     select_category(category_number, driver)
-
-
-# if __name__ == '__main__':
-#     add_category_to_shoot()
