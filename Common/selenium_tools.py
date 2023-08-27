904fd305e0216596a50da1285067b06a32caf996
evgeniy <pavlinbl4@gmail.com>
2023-08-27 17:43:50 +0300

    refactoring

--- Common/selenium_tools.py
+++ Common/selenium_tools.py
@@ -1,12 +1,27 @@
-from Common.kp_image_info_page import image_info_optimization
-from Common.regex_tools import make_text_edit_link
-from Common.soup_tools import get_image_links
+from kp_image_info_page import image_info_optimization
+from make_page_link import make_history_link
+from regex_tools import make_text_edit_link
+from soup_tools import get_image_links
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
 red = '\033[91m'
 green = '\33[32m'
 end = '\033[0m'
+
+
+def open_page(page_link, browser):
+    browser.get(page_link)
+
+
+def work_to_history(driver):
+    driver.find_element(By.ID, 'ctl00_MainContent_ShootInfoForm_FullShootNumber').click()
+    driver.find_element(By.CLASS_NAME, 'history-header').click()
+    first_link = driver.current_url
+    full_link = make_history_link(first_link)  # получаю ссылку на страницу с подробной историей
+    driver.get(full_link)  # открывается новая вкладка, нужно перейти на нее
+    driver.switch_to.window(driver.window_handles[0])
+    return driver.page_source
 
 
 def find_images_by_id(shoot_id, driver):  # авторизация гна главной странице
