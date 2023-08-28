77426179778a309f46c61ac7ae65a2cafc4b51f4
evgeniy <pavlinbl4@gmail.com>
2023-08-28 17:32:38 +0300

    refactoring

--- Fee_in_last_month/images_links.py
+++ Fee_in_last_month/images_links.py
@@ -6,6 +6,5 @@ def get_image_links(html):
     return soup.find_all('table')[9].find('tbody').find_all(title="Добавить кадрировку")
 
 
-
 if __name__ == '__main__':
     get_image_links('/Volumes/big4photo/Documents/Kommersant/FEE/7_July_fee/HTML/source_page_13.07.2023.html')
