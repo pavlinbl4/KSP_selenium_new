95a9af28e1b49dca73eb31b572af445de9241b23
evgeniy <pavlinbl4@gmail.com>
2023-09-15 12:07:43 +0300

    added function to download images from shoot

--- Delete_garbage_photo_REFACTORING/delete_garbage_from_site.py
+++ Delete_garbage_photo_REFACTORING/delete_garbage_from_site.py
@@ -1,21 +1,30 @@
-# удаление мусорных снимков по номеру съемки
+# удаление мусорных снимков по номеру съемки и резервное скачивание съемки на диск
 import time
 from selenium.webdriver.common.by import By
 from Common.authorization import authorization
+from Common.download_to_selected_folder import enable_download
 from Common.get_html import get_html_from_link
+from Common.make_page_link import make_shoot_edit_link
 from Common.regex_tools import make_preview_photo_link
-from Common.selenium_tools import find_images_by_id
+from Common.selenium_tools import find_images_by_id, end_selenium
 from Common.soup_tools import get_image_links
 import sys
 from selenium.common.exceptions import NoSuchElementException, NoSuchWindowException
 from Common.choose_input import clipboard_or_input
+from Common.tk_tools import select_folder_via_gui
 
 
 def main_modul():
     shoot_id = clipboard_or_input()  # shoot_id = 'KSP_017892'
 
-    # authorization on site
+    # select folder to download images
+    image_folder = select_folder_via_gui()
+    download_dir = f'{image_folder}/{shoot_id}'
+
+
+    # authorization on site and enable selected download folder
     driver = authorization()
+    enable_download(driver, download_dir)
 
     # path_to_file = create_report("Kommersant", "deleted_images", shoot_id, [shoot_id, "action"])
     deleted_count = 0
@@ -28,15 +37,24 @@ def main_modul():
     print(f'найдено {number_of_shots} снимков')
     if number_of_shots == 0:
         print("Program terminated")
+        end_selenium(driver)
         sys.exit()
     count = 0
 
     for i in range(number_of_shots):
-        # if I want to download image I must add here this function
+
 
 
         try:
             preview_photo_window_link, image_id, inner_id = make_preview_photo_link(images_links[i].get('href'))
+
+            # if I want to download image I must add here this function
+            shoot_edit_link = make_shoot_edit_link(images_links[i].get('href'))
+            driver.get(shoot_edit_link)
+            driver.find_element(By.CSS_SELECTOR,
+                                f"div.hi-subpanel:nth-child(3) > a:nth-child(4)").click()
+
+
 
             # open preview image window
             driver.get(preview_photo_window_link)
@@ -58,13 +76,13 @@ def main_modul():
         except Exception as ex:
             print(ex)
     try:
-        driver.close()
-        driver.quit()
+        # add sleep to  complete all started downloads
+        time.sleep(5)
+        end_selenium(driver)
 
     except NoSuchWindowException:
         time.sleep(3)
-        driver.close()
-        driver.quit()
+        end_selenium(driver)
 
     print(f'удалено {deleted_count} снимков')
 
