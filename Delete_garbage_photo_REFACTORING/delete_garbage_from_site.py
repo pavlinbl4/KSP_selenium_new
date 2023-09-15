f85ab01fdd8390c28a7105a1207ba519d7a1d920
evgeniy <pavlinbl4@gmail.com>
2023-09-15 10:19:33 +0300

    change some names and add annotations

--- Delete_garbage_photo_REFACTORING/delete_garbage_from_site.py
+++ Delete_garbage_photo_REFACTORING/delete_garbage_from_site.py
@@ -1,25 +1,27 @@
 # удаление мусорных снимков по номеру съемки
-
-
 import time
 from selenium.webdriver.common.by import By
-from Common.authorization import autorization
-from Common.create_XLXS_report_file import create_report
+from Common.authorization import authorization
 from Common.get_html import get_html_from_link
-from Common.regex_tools import make_text_edit_link
+from Common.regex_tools import make_preview_photo_link
 from Common.selenium_tools import find_images_by_id
 from Common.soup_tools import get_image_links
-from Common.write_to_xlsx import write_to_xlsx
 import sys
 from selenium.common.exceptions import NoSuchElementException, NoSuchWindowException
-from Common.choose_input import chose_input
+from Common.choose_input import clipboard_or_input
 
 
 def main_modul():
-    path_to_file = create_report("Kommersant", "deleted_images", shoot_id, [shoot_id, "action"])
+    shoot_id = clipboard_or_input()  # shoot_id = 'KSP_017892'
+
+    # authorization on site
+    driver = authorization()
+
+    # path_to_file = create_report("Kommersant", "deleted_images", shoot_id, [shoot_id, "action"])
     deleted_count = 0
-    link = find_images_by_id(shoot_id, browser)
-    html = get_html_from_link(link, browser)  # получаю html открытой страницы
+
+    link = find_images_by_id(shoot_id, driver)
+    html = get_html_from_link(link, driver)  # получаю html открытой страницы
     images_links = get_image_links(html)  # получаю список ссылок редактирование изображения
     number_of_shots = len(images_links)
 
@@ -30,39 +32,42 @@ def main_modul():
     count = 0
 
     for i in range(number_of_shots):
+        # if I want to download image I must add here this function
+
+
         try:
-            image_edit_link, image_id, inner_id = make_text_edit_link(images_links[i].get('href'))
-            browser.get(image_edit_link)
+            preview_photo_window_link, image_id, inner_id = make_preview_photo_link(images_links[i].get('href'))
+
+            # open preview image window
+            driver.get(preview_photo_window_link)
             time.sleep(1)
+
             try:
-                browser.find_element(By.CSS_SELECTOR, '.td-del > a:nth-child(1) > img').click()
-                confirm = browser.switch_to.alert
+                # find red ring shortcut to delete image
+                driver.find_element(By.CSS_SELECTOR, '.td-del > a:nth-child(1) > img').click()
+                confirm = driver.switch_to.alert
                 confirm.accept()
+                time.sleep(1)
 
                 count += 1
-                write_to_xlsx(image_id, 'deleted', shoot_id, count, path_to_file)
                 print(f'image {image_id} deleted')
                 deleted_count += 1
             except NoSuchElementException:
-                write_to_xlsx(image_id, "was published and can't be delete", shoot_id, count, path_to_file)
                 print(f"Image {image_id} was published and can't be delete")
                 continue
         except Exception as ex:
             print(ex)
     try:
-        browser.close()
-        browser.quit()
+        driver.close()
+        driver.quit()
 
     except NoSuchWindowException:
         time.sleep(3)
-        browser.close()
-        browser.quit()
+        driver.close()
+        driver.quit()
 
     print(f'удалено {deleted_count} снимков')
 
 
 if __name__ == '__main__':
-    shoot_id = chose_input()  # 'KSP_016180'
-
-    browser = autorization()
     main_modul()
