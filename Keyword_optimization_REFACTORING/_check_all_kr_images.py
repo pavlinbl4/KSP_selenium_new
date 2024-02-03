480801536323a2c67c9168897ba3de0c51691501
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-02-03 16:19:54 +0300

    refactoring

--- Keyword_optimization_REFACTORING/_check_all_kr_images.py
+++ Keyword_optimization_REFACTORING/_check_all_kr_images.py
@@ -1,16 +1,18 @@
-from Common.authorization import autorization
-from Common.selenium_tools import check_keywords_number, images_rotator, end_selenium, find_images_by_id
+
+from Common.selenium_tools import number_of_founded_images, images_rotator, end_selenium, \
+    find_all_images_on_site_by_shoot_id_or_keyword
+from kp_selenium_tools.authorization import AuthorizationHandler
 
 
 def main():
-    # 1 autorization on site
-    driver = autorization()
+    # 1 authorization on site
+    driver = AuthorizationHandler().authorize()
 
     # 2 find all my KR images
-    find_images_by_id(shoot_id='', driver=driver)
+    find_all_images_on_site_by_shoot_id_or_keyword(shoot_id='', driver=driver, only_kr=True)
 
-    # 3 take number of images from site, if keyword empty - all images
-    keyword_link, images_number = check_keywords_number(keyword='', driver=driver)
+    # 3 take number of images from site
+    keyword_link, images_number = number_of_founded_images(keyword='', driver=driver)
 
     # 4 check all find images
     images_rotator(images_number, keyword_link, driver)
