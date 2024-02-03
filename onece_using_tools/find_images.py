5fa06127a8d2db2abe4398823acb2a17c2518e02
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-02-03 16:29:06 +0300

    refactoring

--- onece_using_tools/find_images.py
+++ onece_using_tools/find_images.py
@@ -1,25 +1,24 @@
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import Select
-# from Common.authorization import autorization
-#
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
-# def find_images_by_id(shoot_id, driver):  # авторизация гна главной странице
-#
-#     driver.find_element(By.CSS_SELECTOR, '#au').clear()
-#     driver.find_element(By.CSS_SELECTOR, '#au').send_keys('Евгений Павленко')
-#
-#     driver.find_element(By.CSS_SELECTOR, '#code').clear()
-#     driver.find_element(By.CSS_SELECTOR, '#code').send_keys(shoot_id)
-#     driver.find_element(By.CSS_SELECTOR, '#lib0').click()
-#     select = Select(driver.find_element(By.NAME, 'ps'))
-#     select.select_by_value('100')
-#     driver.find_element(By.CSS_SELECTOR, '#searchbtn').click()
-#     link = driver.current_url
-#     return link
-#
-#
-# if __name__ == '__main__':
-#     driver = autorization()
-#     find_images_by_id('KSP_017270', driver)
-#     driver.close()
-#     driver.quit()
+from kp_selenium_tools.authorization import AuthorizationHandler
+
+
+def find_images_by_id(shoot_id, driver):  # авторизация гна главной странице
+
+    driver.find_element(By.CSS_SELECTOR, '#au').clear()
+    driver.find_element(By.CSS_SELECTOR, '#au').send_keys('Евгений Павленко')
+
+    driver.find_element(By.CSS_SELECTOR, '#code').clear()
+    driver.find_element(By.CSS_SELECTOR, '#code').send_keys(shoot_id)
+    driver.find_element(By.CSS_SELECTOR, '#lib0').click()
+    select = Select(driver.find_element(By.NAME, 'ps'))
+    select.select_by_value('100')
+    driver.find_element(By.CSS_SELECTOR, '#searchbtn').click()
+    link = driver.current_url
+    return link
+
+
+if __name__ == '__main__':
+    driver_t = AuthorizationHandler().authorize()
+    find_images_by_id('KSP_017270', driver_t)
