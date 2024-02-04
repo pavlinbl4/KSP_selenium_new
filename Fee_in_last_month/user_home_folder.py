0dda11ce4558cccfa7a2e65c5e4c0f0e01ade8bf
evgeniy <pavlinbl4@gmail.com>
2024-02-04 13:01:11 +0300

    create subfolder in Kommersant folder and return path to it

--- Fee_in_last_month/user_home_folder.py
+++ Fee_in_last_month/user_home_folder.py
@@ -1,26 +1,18 @@
 from pathlib import Path
+import os
 
 
 class HomeFolder:
-    def __init__(self):
-        self.user_home_directory = Path.home()
-        self.list_of_files = Path.glob(Path.home(), '*')
-        self.folders_voc = {}
-        count = 0
-        for i in self.list_of_files:
-            if Path(i).is_dir() and Path(i).name.isalnum():
-                self.folders_voc[count] = Path(i).name
-                count += 1
-
-    def make_path(self, subfolder_name, folder_number):
+    def __init__(self, subfolder_name):
         self.subfolder_name = subfolder_name
-        # self.folder_number = folder_number
-        return f'{home.user_home_directory}/{home.folders_voc[2]}/{home.subfolder_name}'
+        self.user_home_directory = f'{Path.home()}/Documents/Kommersant'
 
-    def add_subfolder_to_kommersant(self, subfolder_name):
-        self.subfolder_name = subfolder_name
-        return f'{home.user_home_directory}/Documents/Kommersant/{home.subfolder_name}'
+    def add_subfolder(self):
+        path = os.path.join(self.user_home_directory, self.subfolder_name)
+        os.makedirs(path, exist_ok=True)
+        return f"{self.user_home_directory}/{self.subfolder_name}"
 
+if __name__ == '__main__':
 
-home = HomeFolder()
-home.add_subfolder_to_kommersant('add_subfolder_to_kommersant')
+    folder = HomeFolder('add_subfolder_to_kommersant')
+    print(folder.add_subfolder())
