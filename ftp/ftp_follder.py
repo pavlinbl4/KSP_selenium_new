b71ba9dd11e119c309240008c16e9b747677d3e6
evgeniy <pavlinbl4@gmail.com>
2024-02-07 21:54:38 +0300

    refactoring

--- ftp/ftp_follder.py
+++ ftp/ftp_follder.py
@@ -2,26 +2,26 @@ import ftplib
 import os
 from dotenv import load_dotenv
 
-def create_ftp_folder(shoot_id):
 
+def create_ftp_folder(shoot_id):
     load_dotenv()
 
-    FTP_LOGIN = os.environ.get('FTP_LOGIN')
-    FTP_PASS = os.environ.get('FTP_PASS')
+    ftp_login = os.environ.get('FTP_LOGIN')
+    ftp_pass = os.environ.get('FTP_PASS')
 
-
-
-    ftp = ftplib.FTP('ftp.kommersant.ru', FTP_LOGIN, FTP_PASS, timeout=None)
+    # noinspection PyTypeChecker
+    ftp = ftplib.FTP('ftp.kommersant.ru', ftp_login, ftp_pass, timeout=1)
     ftp.encoding = "cp1251"
     ftp.cwd("/PHOTO/INBOX/SHOOTS/Pavlenko_Evgenij_2571")
+
     try:
         ftp.mkd(shoot_id)
         print("try")
-    except:
+    except ftplib.error_perm:
         print("550 Cannot create a file when that file already exists")
 
     finally:
-    # ftp.login() # default, i.e.: user anonymous, passwd anonymous@
+        # ftp.login() # default, i.e.: user anonymous, passwd anonymous@
         '230 Guest login ok, access restrictions apply.'
         print(ftp.retrlines('LIST'))  # list directory contents
 
@@ -29,6 +29,6 @@ def create_ftp_folder(shoot_id):
         ftp.quit()
         '221 Goodbye.'
 
-if __name__ == '__main__':
-    create_ftp_folder()
 
+if __name__ == '__main__':
+    create_ftp_folder('KSP_017764')
