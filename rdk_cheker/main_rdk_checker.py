a821fd7eba0dc86360a72bae75fe1716786780bd
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-02-13 22:06:53 +0300

    added functions to check article status adn send message to telegtram, and function to delete old pickle files

--- rdk_cheker/main_rdk_checker.py
+++ rdk_cheker/main_rdk_checker.py
@@ -2,7 +2,6 @@ import re
 import pickle
 
 from selenium import webdriver
-import time
 from datetime import date
 
 import os
@@ -10,6 +9,10 @@ from dotenv import load_dotenv
 
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.service import Service
+
+from Telegramm_message.send_message_to_telegram import send_telegram_message
+from rdk_cheker.check_article_status import check_article_status
+from rdk_cheker.delete_old_pickle import delete_old_pickle
 
 service = Service(GeckoDriverManager().install())
 driver = webdriver.Firefox(service=service)
@@ -44,12 +47,14 @@ def get_today_link(all_spans):
 
 
 def get_article_status():
+    today_filename = date.today().strftime("%d_%m_%y")
+    delete_old_pickle(today_filename)
     all_spans = get_spans()
     articles_dict = {}
-    if os.path.exists('data.pickle'):
-        with open('data.pickle', 'rb') as data_file:
+    if os.path.exists(f'{today_filename}.pickle'):
+        with open(f'{today_filename}.pickle', 'rb') as data_file:
             articles_dict = pickle.load(data_file)
-    print(articles_dict)
+    # print(articles_dict)
     today_link = get_today_link(all_spans)
     driver.get(today_link)  # '//tr[@class="mapLO"][2]/td[6]/img'
     work_map = driver.find_elements('xpath', '//tr[@class="mapLO"]')
@@ -61,12 +66,13 @@ def get_article_status():
         if len(article_name) > 0 and article_name not in articles_dict:
             print(article_name)
             print(article_status)
+            send_telegram_message(f'{article_name} - {article_status}')
             articles_dict[article_name] = article_status
-            with open('data.pickle', 'wb') as f:
+            with open(f'{today_filename}.pickle', 'wb') as f:
                 pickle.dump(articles_dict, f)
+        else:
+            check_article_status(article_name, article_status, today_filename)
 
-
-    time.sleep(3)
     driver.close()
     driver.quit()
 
@@ -74,6 +80,3 @@ def get_article_status():
 if __name__ == '__main__':
     get_article_status()
     # print(article_status())
-
-
-
