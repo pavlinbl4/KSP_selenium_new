f5f1ae943d4623a76cc115a3ca2bc8fbedb76bfb
evgeniy <pavlinbl4@gmail.com>
2024-03-11 11:36:44 +0300

    unused functions were committed

--- Fee_in_last_month/check_published_images.py
+++ Fee_in_last_month/check_published_images.py
@@ -1,18 +1,18 @@
+from Common.write_xlsx import write_to_file, write_xlsx_single_sheet
 from published_images import publication_info, published_for_all_time
-from write_xlsx import write_to_file, write_xlsx_single_sheet
 
 red = '\033[91m'
 green = '\33[32m'
 end = '\033[0m'
 
 
-def images_in_cycle(images_voc, path_to_file, check_date):
-    count = 0
-    print(images_voc)
-    for k in images_voc:
-        count += 1
-        image_info = publication_info(k, count, check_date)
-        write_to_file(path_to_file, image_info, count, check_date)
+# def images_in_cycle(images_voc, path_to_file, check_date):
+#     count = 0
+#     print(images_voc)
+#     for k in images_voc:
+#         count += 1
+#         image_info = publication_info(k, count, check_date)
+#         write_to_file(path_to_file, image_info, count, check_date)
 
 
 def one_day_images_cycle(images_voc, i, path_to_file, count, photographer):  # # проверяю каждый снимок из "засланных"
@@ -24,9 +24,9 @@ def one_day_images_cycle(images_voc, i, path_to_file, count, photographer):  # #
     return count
 
 
-def image_id_cycle(images_voc):
-    count = 0
-    for k in images_voc:  # на самом деле снимок один и цикл не нужен - переделать
-        count += 1
-        image_info = published_for_all_time(k)
-    return image_info
+# def image_id_cycle(images_voc):
+#     count = 0
+#     for k in images_voc:  # на самом деле снимок один и цикл не нужен - переделать
+#         count += 1
+#         return published_for_all_time(k)
+
