0b1b30bb849520000cbe915045470ab2fd2b56c8
evgeniy <pavlinbl4@gmail.com>
2024-03-11 12:11:26 +0300

    clear from old commented lines

--- Fee_in_last_month/check_published_images.py
+++ Fee_in_last_month/check_published_images.py
@@ -6,15 +6,6 @@ green = '\33[32m'
 end = '\033[0m'
 
 
-# def images_in_cycle(images_voc, path_to_file, check_date):
-#     count = 0
-#     print(images_voc)
-#     for k in images_voc:
-#         count += 1
-#         image_info = publication_info(k, count, check_date)
-#         write_to_file(path_to_file, image_info, count, check_date)
-
-
 def one_day_images_cycle(images_voc, i, path_to_file, count, photographer):  # # проверяю каждый снимок из "засланных"
     print(f"{red} in  {i} - {len(images_voc)} were send to publication{end}")
     for k in images_voc:  # k - внутренний id снимка
@@ -22,11 +13,4 @@ def one_day_images_cycle(images_voc, i, path_to_file, count, photographer):  # #
         used_images = publication_info(k, count, i)  # получаю словарь с данными о всех публикациях данного снимка
         write_xlsx_single_sheet(path_to_file, used_images, photographer)  # записываю данную информацию в файл отчета
     return count
-
-
-# def image_id_cycle(images_voc):
-#     count = 0
-#     for k in images_voc:  # на самом деле снимок один и цикл не нужен - переделать
-#         count += 1
-#         return published_for_all_time(k)
 
