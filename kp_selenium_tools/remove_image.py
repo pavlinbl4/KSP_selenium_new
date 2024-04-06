7b6b37184d5ca1676e2df48c897f121b704b532e
evgeniy <pavlinbl4@gmail.com>
2024-04-06 22:16:53 +0300

    image removal from kp archive

--- kp_selenium_tools/remove_image.py
+++ kp_selenium_tools/remove_image.py
@@ -1,21 +1,25 @@
-
-
 import time
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-def delete_image_in_kp_photo_archive(driver,count, image_id, deleted_count):
+def delete_image_in_kp_photo_archive(driver, count, image_id, deleted_count):
     try:
         # find red ring shortcut to delete image
         driver.find_element(By.CSS_SELECTOR, '.td-del > a:nth-child(1) > img').click()
-        confirm = driver.switch_to.alert
-        confirm.accept()
+
+        # Wait for the alert to be present and then accept it
+        wait = WebDriverWait(driver, 10)
+        alert = wait.until(EC.alert_is_present())
+        alert.accept()
+
         time.sleep(1)
 
         count += 1
         print(f'image {image_id} deleted')
         deleted_count += 1
     except NoSuchElementException:
-        print(f"Image {image_id} was published and can't be delete")
+        print(f"Image {image_id} was published and can't be deleted")
     return count, deleted_count
